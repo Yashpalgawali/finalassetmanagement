86b5b5e5f980c0f92233ee7747412c35516213a0
YashpalGawali <gawaliyashpal@gmail.com>
2023-12-08 17:39:08 +0530

    08 Dec Sec Commit

--- src/app/MyComponents/Asset/retrieve-assets/retrieve-assets.component.ts
+++ src/app/MyComponents/Asset/retrieve-assets/retrieve-assets.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AssignedAssets } from 'src/Models/AssignedAssets';
 import { AssetService } from 'src/app/Services/asset.service';
+import { AssignedAssetService } from 'src/app/Services/assigned-asset.service';
 
 @Component({
   selector: 'app-retrieve-assets',
@@ -10,8 +11,10 @@ import { AssetService } from 'src/app/Services/asset.service';
 })
 export class RetrieveAssetsComponent {
 
-  constructor(private assetserv : AssetService,private route : ActivatedRoute, private router : Router) {}
+  constructor(private assetserv : AssetService,private route : ActivatedRoute, private router : Router,
+              private assignassetserv : AssignedAssetService) { }
   assigned_assets :  AssignedAssets = new AssignedAssets(); 
+  
   ngOnInit(): void {
     this.assigned_assets.emp_id= this.route.snapshot.params['id'];
     
--- src/app/Services/assigned-asset.service.ts
+++ src/app/Services/assigned-asset.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { GlobalComponent } from '../GlobalComponents';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssignedAssetService {
 
-  constructor() { }
+  app_url = GlobalComponent.base_url;
+  base_url= this.app_url+"asset/";
+  
+  constructor(private http : HttpClient) { }
+
+  
 }
