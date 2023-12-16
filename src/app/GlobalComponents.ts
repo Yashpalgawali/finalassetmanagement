c63dc975f364b844ba81c1fdbceb483b7a7a16de
YashpalGawali <gawaliyashpal@gmail.com>
2023-12-16 17:35:03 +0530

    16 Dec Sec Commit

--- src/app/GlobalComponents.ts
+++ src/app/GlobalComponents.ts
@@ -1,5 +1,5 @@
 export class GlobalComponent {
 
-    public static base_url = "http://localhost:7979/";
-    //public static base_url = "https://mycodenet.jvmhost.net/appointment/";
+   // public static base_url = "http://localhost:7979/";
+    public static base_url = "https://mycodenet.jvmhost.net/assetmanagement/";
   }
